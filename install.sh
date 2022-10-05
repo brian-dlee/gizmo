d40c0c78ba7f9febc5eabe14ee89dfff26efa609
Brian Lee <briandl92391@gmail.com>
2022-10-05 10:33:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ else
   git pull
 fi
 
-cat >"$HOME/.gizmo/exports.sh" <<"EOF"
+cat >"$HOME/.gizmo/init.sh" <<"EOF"
 export GIZMO_HOME="$HOME/.gizmo"
 export GIZMO_BIN="$GIZMO_HOME/bin"
 export GIZMO_SRC="$GIZMO_HOME/src"
@@ -23,3 +23,19 @@ EOF
 
 chmod +x "$GIZMO_SRC/docker/build/ecr.sh"
 ln -s "$GIZMO_SRC/docker/build/ecr.sh" "$GIZMO_BIN/gizmo-ecr"
+
+case $(basename "$SHELL") in
+zsh)
+  if ! grep ".gizmo/init.sh" "$HOME/.zshrc" >/dev/null; then
+    echo "Add `source $HOME/.gizmo/init.sh` to your .zshrc file to complete installation." >&2
+  fi
+  ;;
+bash)
+  if ! grep ".gizmo/init.sh" "$HOME/.bashrc" >/dev/null; then
+    echo "Add `source $HOME/.gizmo/init.sh` to your .bashrc file to complete installation." >&2
+  fi
+  ;;
+*)
+  echo "Add `source $HOME/.gizmo/init.sh` to your shell configuration file to complete installation." >&2
+  ;;
+esac
